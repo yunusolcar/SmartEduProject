95a5636c0dbcb97b3a7e2168740818eb89ce926e
Yunus <yunusolcar@hotmail.com>
2023-05-30 19:36:44 +0300

    First Lesson

--- app.js
+++ app.js
@@ -2,5 +2,5 @@ const express = require('express')
 const app = express()
 const port = 5000;
 
-app.get('/', (req, res) => res.statusCode(200).send('Index Page'))
+app.get('/', (req, res) => res.status(200).send('Index Page'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
