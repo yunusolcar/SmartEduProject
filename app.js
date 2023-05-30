f09090ddf21833c18ae754f7aeecd28e3d13f70f
Yunus <yunusolcar@hotmail.com>
2023-05-30 19:32:47 +0300

    Main Files

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = 5000;
 
 app.get('/', (req, res) => res.statusCode(200).send('Index Page'))
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
