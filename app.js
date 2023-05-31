5ae8e9f8f0a68eb770af2347a93a38aea7af19ba
Yunus <yunusolcar@hotmail.com>
2023-05-31 16:18:07 +0300

    Lesson 2

--- app.js
+++ app.js
@@ -1,6 +1,26 @@
-const express = require('express')
-const app = express()
-const port = 5000;
+const express = require('express');
+const app = express();
+const ejs = require('ejs');
 
-app.get('/', (req, res) => res.status(200).send('Index Page'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+//Template Engine
+app.set("view engine", "ejs");
+
+//Middleware
+app.use(express.static("public"));
+
+//Routes
+app.get('/', (req, res) => {
+     res.status(200).render('index', {
+          page_name: "index"
+     })
+});
+app.get('/about', (req, res) => {
+     res.status(200).render('about', {
+          page_name: "about"
+     })
+});
+//Port
+const port = 5000;
+app.listen(port, () => {
+     console.log(`Example app listening on port ${port}!`)
+});
