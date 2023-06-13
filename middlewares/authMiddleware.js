c626ed1afffb022a51179433c49bcd7cb4d0f577
Yunus <yunusolcar@hotmail.com>
2023-06-13 11:18:56 +0300

    Lesson 13

---
+++ middlewares/authMiddleware.js
@@ -0,0 +1,13 @@
+const User = require('../models/User');
+
+module.exports = (req, res, next) => {
+    User.findById(req.session.userID)
+        .then(user => {
+            if (!user) {
+                return res.redirect('/login')
+            }
+            next();
+        }).catch(err => {
+            console.log(err)
+        })
+};
