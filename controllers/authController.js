889fa356f0b39506d061dfb12e2478dce927fdf7
Yunus <yunusolcar@hotmail.com>
2023-06-14 08:47:03 +0300

    Lesson 16

--- controllers/authController.js
+++ controllers/authController.js
@@ -2,6 +2,7 @@ const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const session = require('express-session');
 const Category = require('../models/Category');
+const Course = require('../models/Course');
 
 exports.createUser = async (req, res) => {
     try {
@@ -61,9 +62,13 @@ exports.getDashboardPage = async (req, res) => {
         _id: req.session.userID
     })
     const categories = await Category.find();
+    const courses = await Course.find({
+        user: req.session.userID
+    })
     res.status(200).render('dashboard', {
         page_name: 'dashboard',
         user,
-        categories
+        categories,
+        courses
     })
 };
--- controllers/courseController.js
+++ controllers/courseController.js
@@ -3,7 +3,12 @@ const Category = require('../models/Category');
 
 exports.createCourse = async (req, res) => {
      try {
-          const course = await Course.create(req.body);
+          const course = await Course.create({
+               name: req.body.name,
+               description: req.body.description,
+               category: req.body.category,
+               user: req.session.userID
+          });
           res.status(201).redirect('/courses');
      } catch (error) {
           res.status(400).json({
@@ -19,7 +24,7 @@ exports.getAllCourses = async (req, res) => {
           const categorySlug = req.query.categories;
           const category = await Category.findOne({
                slug: categorySlug
-          });
+          }).populate
 
           let filter = {};
           if (categorySlug) {
@@ -46,7 +51,7 @@ exports.getCourse = async (req, res) => {
      try {
           const course = await Course.findOne({
                slug: req.params.slug
-          })
+          }).populate('user')
           res.status(200).render('course', {
                course,
                page_name: 'courses'
