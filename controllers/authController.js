5b49e03d1fa87a0e28b6eb85a27b64f78fd74e5d
Yunus <yunusolcar@hotmail.com>
2023-06-14 07:32:54 +0300

    Lesson 15

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,6 +1,7 @@
 const User = require('../models/User');
 const bcrypt = require('bcrypt');
 const session = require('express-session');
+const Category = require('../models/Category');
 
 exports.createUser = async (req, res) => {
     try {
@@ -59,8 +60,10 @@ exports.getDashboardPage = async (req, res) => {
     const user = await User.findOne({
         _id: req.session.userID
     })
+    const categories = await Category.find();
     res.status(200).render('dashboard', {
         page_name: 'dashboard',
-        user
+        user,
+        categories
     })
 };
